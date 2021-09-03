f75ea4403c0b894c8ecd1750ebe6ffb74bac1058
Steve-njeri <stevenjeri254@gmail.com>
2021-09-03 23:36:51 +0300

    constructed a new instance of the Intent class

--- app/src/main/java/com/stephen/newssearch/MainActivity.java
+++ app/src/main/java/com/stephen/newssearch/MainActivity.java
@@ -2,6 +2,7 @@ package com.stephen.newssearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mFindSearchNewsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+                startActivity(intent);
             }
         });
 
