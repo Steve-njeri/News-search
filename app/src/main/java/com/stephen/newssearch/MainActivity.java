354b04993c12d25cae23452b09c0441919da377d
Steve-njeri <stevenjeri254@gmail.com>
2021-09-04 01:19:11 +0300

    Passing Data with Intent Extras and Gathering Data from Intent Extras

--- app/src/main/java/com/stephen/newssearch/MainActivity.java
+++ app/src/main/java/com/stephen/newssearch/MainActivity.java
@@ -6,24 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindSearchNewsButton;
+    private EditText mNewsEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mNewsEditText = (EditText) findViewById(R.id.newsEditText);
         mFindSearchNewsButton = (Button)findViewById(R.id.findSearchNewsButton);
         mFindSearchNewsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
+                String news = mNewsEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+                intent.putExtra("news", news);
                 startActivity(intent);
             }
         });
-
     }
 }
--- app/src/main/java/com/stephen/newssearch/NewsActivity.java
+++ app/src/main/java/com/stephen/newssearch/NewsActivity.java
@@ -2,13 +2,22 @@ package com.stephen.newssearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NewsActivity extends AppCompatActivity {
+    private TextView mNewsTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        mNewsTextView = (TextView) findViewById(R.id.newsEditText);
+
+        Intent intent = getIntent();
+        String news = intent.getStringExtra("news");
+        mNewsTextView.setText("Here are all the latest news: " + news);
     }
 }
