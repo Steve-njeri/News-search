d75467fda9956fb2cf472b8287bdf55464e442c0
Steve-njeri <stevenjeri254@gmail.com>
2021-09-03 21:55:08 +0300

    added a click listener which will be triggered when our button is touched

--- app/src/main/java/com/stephen/newssearch/MainActivity.java
+++ app/src/main/java/com/stephen/newssearch/MainActivity.java
@@ -3,12 +3,24 @@ package com.stephen.newssearch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindSearchNewsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindSearchNewsButton = (Button)findViewById(R.id.findSearchNewsButton);
+        mFindSearchNewsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //do something
+            }
+        });
+
     }
 }
