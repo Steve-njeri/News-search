fbdd6e8089a1a909e52f89e72a6001c9af24d1df
Steve-njeri <stevenjeri254@gmail.com>
2021-09-05 16:56:32 +0300

    added an OnItemClickListener that will display a toast containing the news details when a list item is clicked

--- app/src/main/java/com/stephen/newssearch/NewsActivity.java
+++ app/src/main/java/com/stephen/newssearch/NewsActivity.java
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewsActivity extends AppCompatActivity {
-    private String[] news = new String[] {"Mi Mero Mole", "Mother's Bistro",
-            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
-            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
-            "Lardo", "Portland City Grill", "Fat Head's Brewery",
-            "Chipotle", "Subway"};
+    private TextView mNewsTextView;
     private ListView mListView;
+    private String[] news = new String[] {
+            "Anti-Abortion Whistleblowing Site Gets New Home With Provider Known for Hosting Extremists - The Daily Beast",
+            "Biden promised ISIS-K will 'pay.' Having no US troops in Afghanistan makes that harder - CNN",
+            "Religious exemptions to vaccine mandates could test 'sincerely held beliefs' - NBC News",
+            "Afghanistan: 'Everyone got it wrong' on Taliban takeover - armed forces chief - BBC News",
+            "Five sailors declared dead in Navy helicopter crash off Southern California - USA TODAY",
+            "New York City was never built to withstand a deluge like the one Ida delivered. It showed. - CNN",
+            "Child Covid-19 Cases Rise in States Where Schools Opened Earliest - The Wall Street Journal",
+            "Gavin Newsom bets fear can fend off voters' fury at his misrule in CA recall election - New York Post",
+            "On Grand Isle, a Fragile Spot Off Louisiana, Vast Damage - The New York Times",
+            "Identities released of 3 people found dead after Millcreek standoff - KUTV 2News"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +32,21 @@ public class NewsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news);
 
         mListView = (ListView) findViewById(R.id.listView);
+        mNewsTextView = (TextView) findViewById(R.id.newsTextView);
+
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, news);
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String news = ((TextView)view).getText().toString();
+                Toast.makeText(NewsActivity.this, news, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String news = intent.getStringExtra("news");
+        mNewsTextView.setText("Here are all the latest news: " + news);
     }
 }
