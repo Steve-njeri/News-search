322d87066fe52a64b516a1c0311a66e1dc09f777
Steve-njeri <stevenjeri254@gmail.com>
2021-09-17 17:15:34 +0300

    Created a ValueEventListener

--- app/src/main/java/com/stephen/newssearch/ui/MainActivity.java
+++ app/src/main/java/com/stephen/newssearch/ui/MainActivity.java
@@ -1,18 +1,24 @@
 package com.stephen.newssearch.ui;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.stephen.newssearch.Constants;
 import com.stephen.newssearch.R;
 
@@ -20,10 +26,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private DatabaseReference mSearchedSourceReference;
+
 //    private SharedPreferences mSharedPreferences;
 //    private SharedPreferences.Editor mEditor;
 
-    private DatabaseReference mSearchedSourceReference;
 
     @BindView(R.id.findSearchNewsButton) Button mFindSearchNewsButton;
     @BindView(R.id.sourceEditText) EditText mSourceEditText;
@@ -35,6 +42,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .getInstance()
                 .getReference()
                 .child(Constants.FIREBASE_CHILD_SEARCHED_SOURCE);
+
+        mSearchedSourceReference.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot sourceSnapshot : snapshot.getChildren()) {
+                    String source = sourceSnapshot.getValue().toString();
+                    Log.d("sources updated", "source: " + source);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //update UI here if error occurred.
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
