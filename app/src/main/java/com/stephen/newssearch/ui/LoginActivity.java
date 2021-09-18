938b543e620073c39f19da23acce81aad5dda530
Steve-njeri <stevenjeri254@gmail.com>
2021-09-18 21:17:29 +0300

    created an empty activity in our ui sub-package called LoginActivity

---
+++ app/src/main/java/com/stephen/newssearch/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.stephen.newssearch.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.stephen.newssearch.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
